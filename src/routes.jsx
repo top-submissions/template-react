cc54dc31ed1c83c7532b98c2caec65407e963929
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-13 14:49:51 +0800

    chore(dir): add pages/, add filler files in components/
    what else:
    - refactor: old placeholder files in pages/ (Page => LandingPage)
    - build(routes): update reference to ErrorPage

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router';
 import App from './App';
-import ErrorPage from './components/ErrorPage';
+import ErrorPage from './pages/ErrorPage';
 
 const routes = createBrowserRouter([
   {
